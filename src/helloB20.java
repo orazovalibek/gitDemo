861622d573639c0506f6246db2903e1fe119cf46
orazovalibek <orazalike@gmail.com>
2020-08-01 08:50:09 -0700

    update helloB20.java

--- src/helloB20.java
+++ src/helloB20.java
@@ -1,5 +1,9 @@
 public class helloB20 {
     public static void main(String[] args) {
+
         System.out.println("hello mother fuckers");
+
+        System.out.println("We are going to change our life");
+
     }
 }
